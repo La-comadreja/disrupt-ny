1c484e11303d60eb323afc0bf440cfe2556e715e
Ivan Vashchenko <srochno@gmail.com>
2015-05-03 08:54:26 -0400

    Add the front-end

---
+++ MailApp1/AppRead/App.js
@@ -0,0 +1,52 @@
+﻿/* Common app functionality */
+
+var app = (function () {
+	'use strict';
+
+	var app = {};
+
+	// Common initialization function (to be called from each page)
+	app.initialize = function () {
+		$('body').append(
+			'<div id="notification-message">' +
+				'<div class="padding">' +
+					'<div id="notification-message-close"></div>' +
+					'<div id="notification-message-header"></div>' +
+					'<div id="notification-message-body"></div>' +
+				'</div>' +
+			'</div>');
+
+		$('#notification-message-close').click(function () {
+			$('#notification-message').hide();
+		});
+
+		// After initialization, expose a common notification function
+		app.showNotification = function (header, text) {
+			$('#notification-message-header').text(header);
+			$('#notification-message-body').text(text);
+			$('#notification-message').slideDown('fast');
+		};
+	};
+	
+	app.getConflicts = function () {
+		app.appt = Office.context.mailbox.item;
+		app.apptToSend = {"subject": app.appt.subject, "location": app.appt.location, "start": app.appt.start, "end": app.appt.end};
+
+		$.get( "https://graph.facebook.com/ivan.vaschenko", function( data ) {
+		  $('#gender').text( data.gender );
+		});
+
+		$.get( "http://45.58.34.209/?event=" + app.apptToSend,
+			function( data ) {
+				app.conflicts = data;
+			});
+		console.log(app.conflicts);
+		/*app.conflicts = {"events": {
+				"previous": {"subject": "Disrupt", "location": "Manhattan Center", "start": "2015-05-02T12:30:00Z", "end": "2015-05-03T09:30:00Z"},
+				"subsequent": {"subject": "Drinking", "location": "Grey Lady", "start": "2015-05-03T01:00:00Z", "end": "2015-05-03T02:00:00Z"}
+			}
+		}*/
+	}
+
+	return app;
+})();
---
+++ MailApp1/AppRead/Home/Home.js
@@ -0,0 +1,28 @@
+﻿/// <reference path="../App.js" />
+/*global app*/
+
+(function () {
+	'use strict';
+
+	// The Office initialize function must be run each time a new page is loaded
+	Office.initialize = function (reason) {
+		$(document).ready(function () {
+			app.initialize();
+			app.getConflicts();
+			displayItemDetails();
+		});
+	};
+
+	// Displays the "Subject" and "From" fields, based on the current mail item
+	function displayItemDetails() {
+	
+		$('#start').text( app.appt.start );
+		$('#end').text( app.appt.end );
+		
+		var s = "", prev = app.conflicts.events.previous, next = app.conflicts.events.subsequent;
+		if (prev) s += prev.subject + " at " + prev.location + "<br/>";
+		if (next) s += next.subject + " at " + next.location + "<br/>";
+		
+		$('#conflicts').html( s );
+	}
+})();
