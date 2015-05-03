981829c46502835f4a41ce9db4afa9010eaaf014
Rebecca Anne Sealfon (La-comadreja) <rebecca_sealfon@qualia-media.com>
2015-05-02 23:26:35 -0400

    test atlantic deploy

--- distance_evaluator.rb
+++ distance_evaluator.rb
@@ -5,14 +5,23 @@ require 'rest_client'
 set :server, 'thin'
 
 get '/' do
-  event_list = '{"events": [{"address": "Columbia University, NY", "event_start": "7:00 PM", "event_end": "8:00 PM"}, {"address": "34 Sidney Place, Brooklyn, NY", "event_start": "8:30 PM", "event_end": "9:00 PM"}]}'
-  #params['test']
-  check_for_conflicts_in(event_list)
+  event = params[:event]
+  nearby_events = find_nearby_events(event)
+  #event_list = '{"events": [{"address": "Columbia University, NY", "event_start": "7:00 PM", "event_end": "8:00 PM"}, {"address": "34 Sidney Place, Brooklyn, NY", "event_start": "8:30 PM", "event_end": "9:00 PM"}]}'
+  #check_for_conflicts_in(event_list)
 end
 
-TOKEN = 'L-cMHLjxYWKHH5hByflVAuJp8XTqmsXo0TL0OhVP9cbKqfiX8PGoxZcK59k-rMGczv9HhV3VCGT70qXgq6sv8HnMl2SxH5HMnXfKjL3YzOa2sK_Vo4tOXzzrn2ME55g1Pgxb8-FgAjWKaF5f93fkrg..'
+ESRI_TOKEN = 'L-cMHLjxYWKHH5hByflVAuJp8XTqmsXo0TL0OhVP9cbKqfiX8PGoxZcK59k-rMGczv9HhV3VCGT70qXgq6sv8HnMl2SxH5HMnXfKjL3YzOa2sK_Vo4tOXzzrn2ME55g1Pgxb8-FgAjWKaF5f93fkrg..'
+OUTLOOK_AUTH = 'Basic aXZhbkB0aGVtd29ya3Mub25taWNyb3NvZnQuY29tOlI1a2g1ZHI1'
 
-def check_for_conflicts_in(event_list)
+def find_nearby_events(param)
+  search_start = (DateTime.parse(JSON.parse(param)['start']) - 1).to_s.split('+').first
+  search_end = (DateTime.parse(JSON.parse(param)['end']) + 1).to_s.split('+').first
+  resource = RestClient::Resource.new(outlook_rest_url(search_start, search_end))
+  response = JSON.parse(resource.get(Authorization: OUTLOOK_AUTH))
+end
+
+def check_for_conflicts_in(event_list, previous = false)
   events = JSON.parse(event_list)['events']
   origin = events.first
   origin_coords = geocode(origin['address'])
@@ -26,7 +35,11 @@ def check_for_conflicts_in(event_list)
     travel_time = route([origin_coords, destination_coords])
     conflicts.push(destination) if time_between_events < travel_time
   end
-  { conflicts: conflicts }.to_json
+  { subsequent: conflicts }
+end
+
+def outlook_rest_url(search_start, search_end)
+  "https://outlook.office365.com/api/v1.0/me/calendarview?startDateTime=#{search_start}Z&endDateTime=#{search_end}Z"
 end
 
 def geocode(address)
@@ -38,9 +51,7 @@ def geocode(address)
 end
 
 def route(endpoints)
-  response = RestClient.get("http://route.arcgis.com/arcgis/rest/services/World/Route/NAServer/Route_World/solve?token=#{TOKEN}&stops=#{endpoints.join(';')}&f=json")
+  response = RestClient.get("http://route.arcgis.com/arcgis/rest/services/World/Route/NAServer/Route_World/solve?token=#{ESRI_TOKEN}&stops=#{endpoints.join(';')}&f=json")
   # JSON.parse(response)['directions'].first['features']
   JSON.parse(response)['routes']['features'].first['attributes']['Total_TravelTime'].to_i + 3
 end
-
-# https://outlook.office365.com/api/v1.0/me/calendarview?startDateTime=2015-05-01T01:00:00Z&endDateTime=2015-05-03T23:00:00Z
