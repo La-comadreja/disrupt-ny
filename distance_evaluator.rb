9b9db9db282616346556fc1bff30a238b35a97ab
Rebecca Anne Sealfon (La-comadreja) <rebecca_sealfon@qualia-media.com>
2015-05-03 01:00:18 -0400

    server should be done

--- distance_evaluator.rb
+++ distance_evaluator.rb
@@ -1,41 +1,49 @@
 require 'sinatra'
 require 'thin'
 require 'rest_client'
+require 'json'
 
 set :server, 'thin'
 
 get '/' do
   event = params[:event]
-  nearby_events = find_nearby_events(event)
-  #event_list = '{"events": [{"address": "Columbia University, NY", "event_start": "7:00 PM", "event_end": "8:00 PM"}, {"address": "34 Sidney Place, Brooklyn, NY", "event_start": "8:30 PM", "event_end": "9:00 PM"}]}'
-  #check_for_conflicts_in(event_list)
+  new_event = JSON.parse(event)
+  nearby_events = find_nearby_events(new_event)
+  check_for_conflicts_in(nearby_events, new_event)
 end
 
 ESRI_TOKEN = 'L-cMHLjxYWKHH5hByflVAuJp8XTqmsXo0TL0OhVP9cbKqfiX8PGoxZcK59k-rMGczv9HhV3VCGT70qXgq6sv8HnMl2SxH5HMnXfKjL3YzOa2sK_Vo4tOXzzrn2ME55g1Pgxb8-FgAjWKaF5f93fkrg..'
 OUTLOOK_AUTH = 'Basic aXZhbkB0aGVtd29ya3Mub25taWNyb3NvZnQuY29tOlI1a2g1ZHI1'
 
-def find_nearby_events(param)
-  search_start = (DateTime.parse(JSON.parse(param)['start']) - 1).to_s.split('+').first
-  search_end = (DateTime.parse(JSON.parse(param)['end']) + 1).to_s.split('+').first
+def find_nearby_events(parsed_param)
+  search_start = (DateTime.parse(parsed_param['start']) - 1).to_s.split('+').first
+  search_end = (DateTime.parse(parsed_param['end']) + 1).to_s.split('+').first
   resource = RestClient::Resource.new(outlook_rest_url(search_start, search_end))
-  response = JSON.parse(resource.get(Authorization: OUTLOOK_AUTH))
+  JSON.parse(resource.get(Authorization: OUTLOOK_AUTH))
 end
 
-def check_for_conflicts_in(event_list, previous = false)
-  events = JSON.parse(event_list)['events']
-  origin = events.first
-  origin_coords = geocode(origin['address'])
-  origin_end_time = DateTime.parse("2015-05-03T#{origin['event_end']}")
-  destinations = events[1..-1]
-  conflicts = []
-  destinations.each do |destination|
-    destination_coords = geocode(destination['address'])
-    destination_start_time = DateTime.parse("2015-05-03T#{destination['event_start']}")
-    time_between_events = ((destination_start_time - origin_end_time) * 24 * 60).to_i
-    travel_time = route([origin_coords, destination_coords])
-    conflicts.push(destination) if time_between_events < travel_time
+def check_for_conflicts_in(nearby_events, new_event)
+  previous_conflicts = []
+  subsequent_conflicts = []
+  new_event_start = DateTime.parse(new_event['start'])
+  new_event_end = DateTime.parse(new_event['end'])
+  new_event_coords = geocode(new_event['location'])
+  nearby_events['value'].each do |event|
+    coords = event['Location']['Coordinates']
+    old_event_coords = "#{coords['Longitude']},#{coords['Latitude']}"
+    old_event_start = DateTime.parse(event['Start'])
+    old_event_end = DateTime.parse(event['End'])
+    if old_event_end <= new_event_start
+      # If the previously scheduled event occurred earlier than the new event
+      time_between_events = ((new_event_start - old_event_end) * 24 * 60).to_i
+      previous_conflicts.push(event) if time_between_events < route([old_event_coords, new_event_coords])
+    elsif new_event_end <= old_event_start
+      # If the previously scheduled event occurred later than the new event
+      time_between_events = ((old_event_start - new_event_end) * 24 * 60.to_i)
+      subsequent_conflicts.push(event) if time_between_events < route([new_event_coords, old_event_coords])
+    end
   end
-  { subsequent: conflicts }
+  { events: { previous: previous_conflicts, subsequent: subsequent_conflicts } }.to_json
 end
 
 def outlook_rest_url(search_start, search_end)
@@ -52,6 +60,5 @@ end
 
 def route(endpoints)
   response = RestClient.get("http://route.arcgis.com/arcgis/rest/services/World/Route/NAServer/Route_World/solve?token=#{ESRI_TOKEN}&stops=#{endpoints.join(';')}&f=json")
-  # JSON.parse(response)['directions'].first['features']
   JSON.parse(response)['routes']['features'].first['attributes']['Total_TravelTime'].to_i + 3
 end
